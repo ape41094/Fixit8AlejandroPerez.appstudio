c62a320dfee53eabe84fa22b463d0869bda3ed69
ape41094 <71613459+ape41094@users.noreply.github.com>
2020-10-04 22:52:38 -0500

    Update membership.js

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,15 @@
-/*
+
 let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
+
+
+btnSubmitMember.onclick=function(){
+  members = inptMember.value
+  if (members.includes(InptMember.value))
+  memberLabel.value = "You are a member" + InptMember.value
+  else { 
+   memberLabel.value = "You are not a member" 
+   members.push(Inptmember.value) 
+   console.log(members)}
+  }
+
 
